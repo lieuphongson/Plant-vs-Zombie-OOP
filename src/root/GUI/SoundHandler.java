5eb90febdb0129fc8f630a3bf74897e837ee455f
Vũ Thành Nhân <147362828+VuThanhNhan2003@users.noreply.github.com>
2024-01-16 00:25:44 +0700

    Add files via upload

---
+++ src/root/GUI/SoundHandler.java
@@ -0,0 +1,26 @@
+package root.GUI;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+public class SoundHandler {
+    public static void RunMusic(String path){
+        try {
+            AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File(path));
+            Clip clip = AudioSystem.getClip();
+            clip.open(inputStream);
+            clip.loop(10);
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+}
